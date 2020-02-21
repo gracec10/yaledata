a02cacb2e2f4b332fe2a43f7f5a7da5765da05fe
Grace Cheung <gracecheung@Graces-MacBook-Air.local>
2020-02-20 20:21:02 -0500

    restructured code into functions

--- hw2_data 2/yaledining.py
+++ hw2_data 2/yaledining.py
@@ -4,53 +4,73 @@ import csv
 import graphviz
 from sklearn import tree
 
-#data preprocessing section
+# constants
+TOTAL_DAYS = 28
 
-# parse building_codes.csv to find Branford and Silliman's building codes
-with open('building_codes.csv') as csvfile1:
-    readCSV1 = csv.reader(csvfile1, delimiter=',')
-    for row in readCSV1:
-        if row[1] == "BR":
-            BRCode = row[0]
-        if row[1] == "SM":
-            SMCode = row[0]
+# order of data in files
+# building_codes.csv = id, code, name, type
+# door_data.csv = day, day_of_week, student_id, time_of_day, building, is_dining_hall
 
-# find mean traffic per day for Branford and Silliman each
-totalSwipesBR = 0
-totalSwipesSM = 0
+def get_building_code(bldg_code):
+    # parse building_codes.csv to find building ID for a specified building, given building code (abbreviation) - for ex: BR, SM
+    with open('building_codes.csv') as csvfile1:
+        readCSV1 = csv.reader(csvfile1, delimiter=',')
+        for row in readCSV1:
+            if row[1] == bldg_code:
+              bldg_id = row[0]
+    return (bldg_id) # returns a str
 
-with open('door_data.csv') as csvfile2:
-    readCSV2 = csv.reader(csvfile2, delimiter=',')
-    for row in readCSV2:
-        building = row[4]
-        if building == BRCode:
-            totalSwipesBR = totalSwipesBR + 1
-        if building == SMCode:
-            totalSwipesSM = totalSwipesSM + 1
+def get_mean_traffic(bldg_id):
+    # find mean traffic over the given 28 days for specified building
+    total_swipes = 0
 
-meanTrafficBR = totalSwipesBR / 28 # 28 days total
-meanTrafficSM = totalSwipesSM / 28
+    with open('door_data.csv') as csvfile2:
+        readCSV2 = csv.reader(csvfile2, delimiter=',')
+        for row in readCSV2:
+            if row[4] == bldg_id:
+              total_swipes += 1
 
-# constants for mean traffic comparisons
-LOWTRAFFICBR = 0.95 * meanTrafficBR
-HIGHTRAFFICBR = 1.05 * meanTrafficBR
-LOWTRAFFICSM = 0.95 * meanTrafficSM
-HIGHTRAFFICSM = 1.05 * meanTrafficSM
+    mean_traffic = total_swipes / TOTAL_DAYS
+    return (mean_traffic)
 
-# 
+def main():
+    bldg_id = get_building_code("BR")
+    mean_traffic = get_mean_traffic(bldg_id)
 
-# train decision tree
+    print(bldg_id, mean_traffic)
+
+# def assign_training_labels_samples(bldg_id, mean_traffic):
+#     samples = [[0, 0, 0, 0] for x in range(TOTAL_DAYS)] 
+#     # samples[x][0] = day of the week (0-6, Sunday to Saturday)
+#     # samples[x][1] = number of swipes by 11:45am
+#     # samples[x][2] = number of swipes by 12:00pm
+#     # samples[x][3] = number of swipes by 12:15pm
+
+#     labels = [0 for x in range(TOTAL_DAYS)]
+#     # labels to be used: -1 = low traffic, 0 = normal day, 1 = high traffic
+
+#     with open('door_data.csv') as csvfile2:
+#         readCSV3 = csv.reader(csvfile3, delimiter = ',')
+#         for row in readCSV3:
+#             if row[building] == bldg_id:
+#                 samples[row[day]]
+            
+        
+
 
 # prediction for Branford with given data
 # 78 swipes by 11:45am, 131 swipes by 12:00pm, 232 swipes by 12:15pm on a Monday
 
-BRSamples = 
-BRLabels = 
+# BRSamples = 
+# BRLabels = 
 
-clf = tree.DecisionTreeClassifier()
+# clf = tree.DecisionTreeClassifier()
 
-clf_train = clf.fit()
+# clf_train = clf.fit()
 
 
 # prediction for Silliman with given data
 # 90 swipes by 11:45am, 171 swipes by 12:00pm, 230 swipes by 12:15pm on a Sunday
+
+if __name__ == "__main__":
+    main()
